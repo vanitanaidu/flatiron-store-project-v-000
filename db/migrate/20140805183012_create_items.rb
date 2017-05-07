6a3a2d5a698a45ca847d6ce067811ab73102e565
vanitanaidu <vanita112003@gmail.com>
2017-05-07 19:30:53 -0400

    Done.

--- db/migrate/20140805183012_create_items.rb
+++ db/migrate/20140805183012_create_items.rb
@@ -1,5 +1,13 @@
 class CreateItems < ActiveRecord::Migration
-  def change
 
+  def change
+    create_table :items do |t|
+      t.string :title
+      t.decimal :price
+      t.integer :inventory
+      t.integer :category
+
+      t.timestamps null: false
+    end
   end
 end
--- db/migrate/20140805184430_create_line_items.rb
+++ db/migrate/20140805184430_create_line_items.rb
@@ -1,5 +1,14 @@
 class CreateLineItems < ActiveRecord::Migration
+
   def change
+    create_table :line_items do |t|
+      t.integer :quantity
+      t.integer :cart_id
+      t.integer :item_id
+
+
+      t.timestamps null: false
+    end
 
   end
 end
